364fc3e59037dd1fad5fc171c06e6bdd61c0686e
Harol <harolhiguera2005@gmail.com>
2021-01-01 23:53:50 +0900

    test setup

--- Currency ConverterTests/Currency_ConverterTests.swift
+++ Currency ConverterTests/Currency_ConverterTests.swift
@@ -9,7 +9,15 @@ import XCTest
 @testable import Currency_Converter
 
 class Currency_ConverterTests: XCTestCase {
-
+    
+    var dataManager: DataManager!
+    
+    override func setUp() {
+        super.setUp()
+        dataManager = DataManager()
+    }
+    
+    
     override func setUpWithError() throws {
         // Put setup code here. This method is called before the invocation of each test method in the class.
     }
